9fed92d149c10e8c5cbca5881d0f4bc3c44b96af
nicolasprudencio <nicolas-prudencio@hotmail.com>
2024-11-08 21:26:08 -0300

    second

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,14 +6,14 @@
 int main(int argc, char **argv)
 {
 	if (argc != 2) {
-		std::cerr << "Usage: ./rpn [input file]" << std::endl;
+		std::cout << "Usage: ./rpn [input file]" << std::endl;
 		return 1;
 	}
 	try {
 		RPN::validateInputArgument(argv[1]);
 		std::cout << RPN::calculate(argv[1]) << std::endl;
 	} catch (const std::invalid_argument &e) {
-		std::cerr << e.what() << std::endl;
+		std::cout << e.what() << std::endl;
 		return 1;
 	}
 }
